17cba665e72931704bc92a2d6f360e05bed4cbe4
Alexey Zubkov <alexey200223@gmail.com>
2021-10-04 00:29:51 +0300

    Add view for looking at pictures

---
+++ CatsPhotosApp/Controllers/CatsPhotoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CatsPhotoViewController.swift
+//  CatsPhotosApp
+//
+//  Created by TarraeRarae on 03.10.2021.
+//
+
+import UIKit
+
+class CatsPhotoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
