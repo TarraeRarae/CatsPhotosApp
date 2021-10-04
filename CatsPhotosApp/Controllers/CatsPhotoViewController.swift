071a409e47911f57717268dd6a68bcb4903d0eb3
Alexey Zubkov <alexey200223@gmail.com>
2021-10-04 18:51:14 +0300

    Commit

--- CatsPhotosApp/Controllers/CatsPhotoViewController.swift
+++ CatsPhotosApp/Controllers/CatsPhotoViewController.swift
@@ -12,6 +12,8 @@ class CatsPhotoViewController: UIViewController {
 	private var currentCatPhotoData: CatsPhotos
 	private var breedLabel: UILabel = UILabel()
 	private var catImageView: UIImageView = UIImageView()
+	private var favoriteButton: UIButton = UIButton()
+	private var scrollView: UIScrollView = UIScrollView()
 
 	init(_ currentCatPhotoData: CatsPhotos) {
 		self.currentCatPhotoData = currentCatPhotoData
@@ -26,28 +28,29 @@ class CatsPhotoViewController: UIViewController {
 		super.viewDidLoad()
 		view.backgroundColor = .white
 		navigationItem.title = currentCatPhotoData.name
-		setupBreedLabel()
 		setupCatImageView()
+		setupBreedLabel()
+		setupFavoriteButton()
+		setupConstraints()
+	}
+
+	func setupScrollView() {
+		scrollView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+		scrollView.addSubview(catImageView)
 	}
 
 	func setupBreedLabel() {
 		breedLabel.text = currentCatPhotoData.breed
-		breedLabel.frame = CGRect(
-			x: Double(view.bounds.width) * 0.15,
-			y: Double(view.bounds.height) * 0.75,
-			width: Double(view.bounds.width) * 0.5,
-			height: 50
-		)
+//		breedLabel.frame = CGRect(
+//			x: Double(view.bounds.width) * 0.15,
+//			y: Double(view.bounds.height) * 0.75,
+//			width: Double(view.bounds.width) * 0.5,
+//			height: 50
+//		)
 		view.addSubview(breedLabel)
 	}
 
 	func setupCatImageView() {
-		catImageView.frame = CGRect(
-			x: Double(view.bounds.width) * 0.1,
-			y: Double(view.bounds.height) * 0.15,
-			width: Double(view.bounds.width) * 0.8,
-			height: Double(view.bounds.width) * 0.8
-		)
 		let task = URLSession.shared.dataTask(with: currentCatPhotoData.url) { (data, _, error) in
 			guard error == nil,
 						let dataActual = data,
@@ -63,4 +66,41 @@ class CatsPhotoViewController: UIViewController {
 		view.addSubview(catImageView)
 	}
 
+	func setupFavoriteButton() {
+		favoriteButton.frame = CGRect(
+			x: Double(view.bounds.width) * 0.15,
+			y: Double(view.bounds.height) * 0.5,
+			width: 70,
+			height: 70
+		)
+		favoriteButton.setImage(UIImage(systemName: "suit.heart"), for: .normal)
+		favoriteButton.contentHorizontalAlignment = .fill
+		favoriteButton.contentVerticalAlignment = .fill
+		favoriteButton.imageEdgeInsets = UIEdgeInsets(top: 20, left: 6, bottom: 10, right: 10)
+
+		view.addSubview(favoriteButton)
+	}
+	func setupConstraints() {
+		setBreedLabelConstraints()
+		setCatImageViewConstraints()
+	}
+
+	func setBreedLabelConstraints() {
+		breedLabel.translatesAutoresizingMaskIntoConstraints = false
+		NSLayoutConstraint.activate([
+			breedLabel.topAnchor.constraint(equalTo: catImageView.bottomAnchor, constant: 100),
+			breedLabel.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+			breedLabel.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 42),
+			breedLabel.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor)
+		])
+	}
+	func setCatImageViewConstraints() {
+		catImageView.translatesAutoresizingMaskIntoConstraints = false
+		NSLayoutConstraint.activate([
+			catImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+			catImageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 42),
+			catImageView.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.8),
+			catImageView.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.8)
+		])
+	}
 }
